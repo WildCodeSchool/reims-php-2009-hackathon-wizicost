8f038af1aaeedb6d45a5df6ca6b0f1addbd1de55
Axel <axelcroizepro@gmail.com>
2021-01-13 16:09:20 +0100

    added relationship between user & resource

--- src/Entity/Resource.php
+++ src/Entity/Resource.php
@@ -42,6 +42,11 @@ class Resource
      */
     private $name;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="resources")
+     */
+    private $user;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -103,6 +108,18 @@ class Resource
     public function setName(?string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
